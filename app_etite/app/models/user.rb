ee9df8514867646015d298a4f0d98c5e1e4d44c0
James Rutledge <jarutledge2@gmail.com>
2015-06-08 10:28:05 -0500

    created the file structure, added bcrypt functionality

---
+++ app_etite/app/models/user.rb
@@ -0,0 +1,20 @@
+require 'bcrypt'
+
+class User < ActiveRecord::Base
+	has_many :jokes
+
+	# to access :password within the user instance
+	attr_accessor :password_new
+
+	# users.password_hash in the database is a :string
+	include BCrypt
+
+	def password
+		@password ||= Password.new(password_hash)
+	end
+
+	def password=(new_password)
+		@password = Password.create(new_password)
+		self.password_hash = @password
+	end
+end

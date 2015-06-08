3ccb56966b91c3196a90756279993357e7cb14a0
Courtney Regensburger <courtneyregensburger@yahoo.com>
2015-06-08 15:10:49 -0500

    seeds lunch and comment

--- app_etite/db/seeds.rb
+++ app_etite/db/seeds.rb
@@ -68,131 +68,150 @@ alejandra = User.new({
     })
 alejandra.save
 
-
-Lunch.new([
-    {
+#Lunch seeds
+lunch = Lunch.create({
     :food_type => "Home Slice Pizza",
     :rank => 0,
     :user_id => 1
-    },
+    })
 
-    { :food_type =>  "Perry\'s Steakhouse",
+lunch = Lunch.create({
+    :food_type =>  "Perry\'s Steakhouse",
     :rank => 0,
     :user_id => 6
-    },
-    
-    { :food_type => "Petares Burgers",
+    })
+
+lunch = Lunch.create({
+    :food_type => "Petares Burgers",
     :rank => 0,
     :user_id => 7
-    },
-    
-    { :food_type => "Torchy\'s tacos",
+    })
+
+lunch = Lunch.create({
+    :food_type => "Torchy\'s tacos",
     :rank => 0,
     :user_id => 1
-    },
-    
-    { :food_type => "Hopddody Burgers",
+    })
+
+lunch = Lunch.create({
+    :food_type => "Hopddody Burgers",
     :rank => 0,
     :user_id => 3
-    },
-    
-    { :food_type => "Magnolia Cafe South",
+    })
+
+lunch = Lunch.create({
+    :food_type => "Magnolia Cafe South",
     :rank => 0,
     :user_id => 9
-    },
+    })
 
-    { :food_type => "Opal Divine\'s Penn Field",
+lunch = Lunch.create({
+    :food_type => "Opal Divine\'s Penn Field",
     :rank => 0,
     :user_id => 8
-    },
+    })
 
-    { :food_type => "Doc\'s",
+lunch = Lunch.create({
+    :food_type => "Doc\'s",
     :rank => 0,
     :user_id => 11
-    },
+    })
 
-    { :food_type => "Lucy\'s Fried Chicken",
+lunch = Lunch.create({
+    :food_type => "Lucy\'s Fried Chicken",
     :rank => 0,
     :user_id => 7
-    },
+    })
 
-    { :food_type => "El Borrego De Oro",
+lunch = Lunch.create({
+    :food_type => "El Borrego De Oro",
     :rank => 0,
     :user_id => 10
-    }, 
+    })
 
-    { :food_type => "Hill\'s Cafe",
+lunch = Lunch.create({
+    :food_type => "Hill\'s Cafe",
     :rank => 0,
     :user_id => 2
-    },
+    })
 
-    { :food_type => "Little Barrel And Brown",
+lunch = Lunch.create({
+    :food_type => "Little Barrel And Brown",
     :rank => 0,
-    :user_id => 5
-    }
-    ])
-lunch.save
+    :user_id => 5,
+    })
 
-Comment.new([
-    {
+
+#User comments about food
+
+comment = Comment.create({
     :comment => "New York wishes it had this quality pizza.",
     :lunch_id => 1,
     :user_id => 1
-    },
+    })
 
-    { :comment =>  "Always busy, so there is a bit of a wait, but definitely worth it!",
+comment = Comment.create({
+    :comment =>  "Always busy, so there is a bit of a wait, but definitely worth it!",
     :lunch_id => 2,
     :user_id => 6
-    },
-    
-    { :comment => "Perfect burgers, perfect serving size, perfect prices",
+    })
+
+comment = Comment.create({
+    :comment => "Perfect burgers, perfect serving size, perfect prices",
     :lunch_id => 3,
     :user_id => 7
-    },
-    
-    { :comment => "Best tacos in town.",
+    })
+
+comment = Comment.create({
+    :comment => "Best tacos in town.",
     :lunch_id => 4,
     :user_id => 1
-    },
-    
-    { :comment => "The house special was the best.",
+    })
+
+comment = Comment.create({
+    :comment => "The house special was the best.",
     :lunch_id => 5,
     :user_id => 3
-    },
-    
-    { :comment => "Really tasty tea, I will definitely be going here again.",
+    })
+
+comment = Comment.create({
+    :comment => "Really tasty tea, I will definitely be going here again.",
     :lunch_id => 6,
     :user_id => 9
-    },
+    })
 
-    { :comment => "The french fries are the best in town",
+comment = Comment.create({
+    :comment => "The french fries are the best in town",
     :lunch_id => 7,
     :user_id => 8
-    },
+    })
 
-    { :comment => "Awful service, awful food. Do not like.",
+comment = Comment.create({
+    :comment => "Awful service, awful food. Do not like.",
     :lunch_id => 8,
     :user_id => 11
-    },
+    })
 
-    { :comment => "I wish they had a really spicy fried chicken.",
+comment = Comment.create({
+    :comment => "I wish they had a really spicy fried chicken.",
     :lunch_id => 9,
     :user_id => 7
-    },
+    })
 
-    { :comment => "Great service, but the tacos were disappointing.",
+comment = Comment.create({
+    :comment => "Great service, but the tacos were disappointing.",
     :lunch_id => 10,
     :user_id => 10
-    }, 
+    })
 
-    { :comment => "Try the salad, it's so fresh",
+comment = Comment.create({
+    :comment => "Try the salad, it's so fresh",
     :lunch_id => 11,
     :user_id => 2
-    },
+    })
 
-    { :comment => "Awesome beer here",
+comment = Comment.create({
+    :comment => "Awesome beer here",
     :lunch_id => 12,
     :user_id => 5
-    }
-  ])
-comment.save
+    })

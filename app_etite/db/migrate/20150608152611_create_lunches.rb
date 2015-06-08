305cf73b565feac617ead2a70c8fed72bb95c30f
James Rutledge <jarutledge2@gmail.com>
2015-06-08 10:54:54 -0500

    Added to Walker's migrations

--- app_etite/db/migrate/20150608152611_create_lunches.rb
+++ app_etite/db/migrate/20150608152611_create_lunches.rb
@@ -1,9 +1,11 @@
 class CreateLunches < ActiveRecord::Migration
   def change
     create_table :lunches do |t|
-      t.string :name, :null => false
-      t.text :descrip, :null => false
-      t.references :user, index: true, foreign_key: true
+		t.string :food_type, :null => false, index => true
+		t.integer :rank, :null => false
+		t.integer :user_id, :null => false
+
+		t.timestamps null: false
     end
   end
 end

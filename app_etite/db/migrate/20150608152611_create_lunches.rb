346db9b05cdb9b79cebb339bdcc9d0cd1b2aa557
Courtney Regensburger <courtneyregensburger@yahoo.com>
2015-06-08 13:25:09 -0500

    Fixing create lunches migrate

--- app_etite/db/migrate/20150608152611_create_lunches.rb
+++ app_etite/db/migrate/20150608152611_create_lunches.rb
@@ -3,8 +3,6 @@ class CreateLunches < ActiveRecord::Migration
     create_table :lunches do |t|
 		t.string :food_type, :null => false, :index => true
 		t.integer :rank, :null => false
-		t.integer :user_id, :null => false
-
 		t.timestamps null: false
     end
   end

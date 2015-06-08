1cdb7c8639683e15ad9842fe81f2b6da1e4c469a
James <jarutledge2@gmail.com>
2015-06-08 15:20:46 -0500

    Delete 20150608152611_create_lunches.rb

--- app_etite/db/migrate/20150608152611_create_lunches.rb
+++
@@ -1,9 +0,0 @@
-class CreateLunches < ActiveRecord::Migration
-  def change
-    create_table :lunches do |t|
-		t.string :food_type, :null => false, :index => true
-		t.integer :rank, :null => false
-		t.timestamps null: false
-    end
-  end
-end

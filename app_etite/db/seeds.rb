3850e0437def80df44f81382b156e6e690ce4e43
Courtney Regensburger <courtneyregensburger@yahoo.com>
2015-06-08 13:51:00 -0500

    seeds meal and comment seeds updated

--- app_etite/db/seeds.rb
+++ app_etite/db/seeds.rb
@@ -67,3 +67,133 @@ alejandra = User.new({
     :password => "password"
     })
 alejandra.save
+
+
+Lunch.new([
+    {
+    :food_type => "Home Slice Pizza",
+    :rank => 0,
+    :user_id => 1
+    },
+
+    { :food_type =>  "Perry\'s Steakhouse",
+    :rank => 0,
+    :user_id => 6
+    },
+    
+    { :food_type => "Petares Burgers",
+    :rank => 0,
+    :user_id => 7
+    },
+    
+    { :food_type => "Torchy\'s tacos",
+    :rank => 0,
+    :user_id => 1
+    },
+    
+    { :food_type => "Hopddody Burgers",
+    :rank => 0,
+    :user_id => 3
+    },
+    
+    { :food_type => "Magnolia Cafe South",
+    :rank => 0,
+    :user_id => 9
+    },
+
+    { :food_type => "Opal Divine\'s Penn Field",
+    :rank => 0,
+    :user_id => 8
+    },
+
+    { :food_type => "Doc\'s",
+    :rank => 0,
+    :user_id => 11
+    },
+
+    { :food_type => "Lucy\'s Fried Chicken",
+    :rank => 0,
+    :user_id => 7
+    },
+
+    { :food_type => "El Borrego De Oro",
+    :rank => 0,
+    :user_id => 10
+    }, 
+
+    { :food_type => "Hill\'s Cafe",
+    :rank => 0,
+    :user_id => 2
+    },
+
+    { :food_type => "Little Barrel And Brown",
+    :rank => 0,
+    :user_id => 5
+    :comment_id => 
+    }
+    ])
+lunch.save
+
+Comment.new([
+    {
+    :comment => "New York wishes it had this quality pizza.",
+    :lunch_id => 1,
+    :user_id => 1
+    },
+
+    { :comment =>  "Always busy, so there is a bit of a wait, but definitely worth it!",
+    :lunch_id => 2,
+    :user_id => 6
+    },
+    
+    { :comment => "Perfect burgers, perfect serving size, perfect prices",
+    :lunch_id => 3,
+    :user_id => 7
+    },
+    
+    { :comment => "Best tacos in town.",
+    :lunch_id => 4,
+    :user_id => 1
+    },
+    
+    { :comment => "The house special was the best.",
+    :lunch_id => 5,
+    :user_id => 3
+    },
+    
+    { :comment => "Really tasty tea, I will definitely be going here again.",
+    :lunch_id => 6,
+    :user_id => 9
+    },
+
+    { :comment => "The french fries are the best in town",
+    :lunch_id => 7,
+    :user_id => 8
+    },
+
+    { :comment => "Awful service, awful food. Do not like.",
+    :lunch_id => 8,
+    :user_id => 11
+    },
+
+    { :comment => "I wish they had a really spicy fried chicken.",
+    :lunch_id => 9,
+    :user_id => 7
+    },
+
+    { :comment => "Great service, but the tacos were disappointing.",
+    :lunch_id => 10,
+    :user_id => 10
+    }, 
+
+    { :comment => "Try the salad, it's so fresh",
+    :lunch_id => 11,
+    :user_id => 2
+    },
+
+    { :comment => "Awesome beer here",
+    :lunch_id => 12,
+    :user_id => 5
+    }
+  ])
+comment.save

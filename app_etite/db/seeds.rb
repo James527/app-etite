daa97435386453441f4ecaa4286668953fd40a16
James Rutledge <jarutledge2@gmail.com>
2015-06-08 12:20:43 -0500

    Adding Courtney's additions to seeds.rb

--- app_etite/db/seeds.rb
+++ app_etite/db/seeds.rb
@@ -5,3 +5,86 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+james = User.new({
+    :username => "james527",
+    :email => "jamesrutledge@email.com",
+    :password => "password"
+    })
+james.save
+al = User.new({
+    :username => "james527",
+    :email => "jamesrutledge@email.com",
+    :password => "password"
+    })
+al.save
+courtney = User.new({
+    :username => "james527",
+    :email => "jamesrutledge@email.com",
+    :password => "password"
+    })
+courtney.save
+walker = User.new({
+    :username => "james527",
+    :email => "jamesrutledge@email.com",
+    :password => "password"
+    })
+walker.save
+caren = User.new({
+    :username => "carenvaleria",
+    :email => "carenvaleria@email.com",
+    :password => "password"
+    })
+caren.save
+nancy = User.new({
+    :username => "nvalladares",
+    :email => "nancy@email.com",
+    :password => "password"
+    })
+nancy.save
+wendi = User.new({
+    :username => "AreYouCereal",
+    :email => "wendi@email.com",
+    :password => "password"
+    })
+wendi.save
+wes = User.new({
+    :username => "WesHoover",
+    :email => "wes@email.com",
+    :password => "password"
+    })
+wes.save
+zach = User.new({
+    :username => "zfountain",
+    :email => "zachfountain@email.com",
+    :password => "password"
+    })
+zach.save
+alejandra = User.new({
+    :username => "alechapa",
+    :email => "alejandrachapa@email.com",
+    :password => "password"
+    })
+alejandra.save
+
+
+#Lunch seeds
+lunch_list = [
+  [ "Home Slice Pizza", 0, 1],
+  [ "Perry\'s Steakhouse", 0, 2],
+  [ "Petares Burgers", 0, 3],
+  [ "Torchy\'s tacos", 0, 4],
+  [ "Hopddody Burgers", 0, 5],
+  [ "Magnolia Cafe South", 0, 1],
+  [ "Opal Divine\'s Penn Field", 0, 20],
+  [ "Doc\'s", 0, 17],
+  [ "Lucy\'s Fried Chicken", 0, 18],
+  [ "El Borrego De Oro", 0, 11],
+  [ "Hill\'s Cafe", 0, 10],
+  [ "Little Barrel And Brown", 0, 13]
+]
+
+lunch_list.each do |name, rank, user_id|
+  Lunch.create( name: name, rank: rank, user_id: username )
+end

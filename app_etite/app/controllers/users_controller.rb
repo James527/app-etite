c86eedd6bcca7cc8a61e0ca1fd3dd14a33ba42d6
Walker Flieller <wflieller@gmail.com>
2015-06-09 12:36:58 -0500

    Views in sync

---
+++ app_etite/app/controllers/users_controller.rb
@@ -0,0 +1,48 @@
+class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@users = User.all
+	end
+
+
+	def show
+		@user = User.find_by(params[:id])
+	end
+
+	def edit
+		@user = User.find_by(params[:id])
+
+	end
+
+
+	def update
+	    respond_to do |format|
+	      if @user.update(user_params)
+	        format.html { redirect_to @user, notice: 'You have been updated!' }
+	      else
+	        format.html { render :edit }
+	      end
+  		end
+	end
+
+
+	def destroy
+		@user.destroy
+	    respond_to do |format|
+	      format.html { redirect_to users_url, notice: 'User has been successfully deleted!' }
+	    end
+	end
+
+	private
+  	def set_user
+      @user = User.find(params[:id])
+    end
+
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:id, :email, :password)
+    end
+
+end

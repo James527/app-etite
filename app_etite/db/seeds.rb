5f2c36618a763850eb44fcda1ff9b711862615d8
James Rutledge <jarutledge2@gmail.com>
2015-06-09 15:41:28 -0500

    Another migrations & seeds update, took out usernames

--- app_etite/db/seeds.rb
+++ app_etite/db/seeds.rb
@@ -8,61 +8,51 @@
 
 
 james = User.new({
-    :username => "james527",
     :email => "jamesrutledge@email.com",
     :password => "password"
     })
 james.save
 al = User.new({
-    :username => "alwanmo",
     :email => "al@email.com",
     :password => "password"
     })
 al.save
 courtney = User.new({
-    :username => "cregensburger",
     :email => "cregensburger@email.com",
     :password => "password"
     })
 courtney.save
 walker = User.new({
-    :username => "walker",
     :email => "walker@email.com",
     :password => "password"
     })
 walker.save
 caren = User.new({
-    :username => "carenvaleria",
     :email => "carenvaleria@email.com",
     :password => "password"
     })
 caren.save
 nancy = User.new({
-    :username => "nvalladares",
     :email => "nancy@email.com",
     :password => "password"
     })
 nancy.save
 wendi = User.new({
-    :username => "AreYouCereal",
     :email => "wendi@email.com",
     :password => "password"
     })
 wendi.save
 wes = User.new({
-    :username => "WesHoover",
     :email => "wes@email.com",
     :password => "password"
     })
 wes.save
 zach = User.new({
-    :username => "zfountain",
     :email => "zachfountain@email.com",
     :password => "password"
     })
 zach.save
 alejandra = User.new({
-    :username => "alechapa",
     :email => "alejandrachapa@email.com",
     :password => "password"
     })

71ce974e5d9b5c75fa91bc834c2d8455da2ce7e5
Walker Flieller <wflieller@gmail.com>
2015-06-10 15:56:51 -0500

    Committing Profile changes

--- app_etite/db/migrate/20150610192424_add_profile_ref_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddProfileRefToUsers < ActiveRecord::Migration
-  def change
-  	add_reference :users, :profile, index: true, foreign_key: true
-  end
-end
--- app_etite/db/schema.rb
+++ app_etite/db/schema.rb
@@ -37,9 +37,12 @@ ActiveRecord::Schema.define(version: 20150610200739) do
   add_index "lunches", ["user_id"], name: "index_lunches_on_user_id", using: :btree
 
   create_table "profiles", force: :cascade do |t|
-    t.string "first_name", null: false
-    t.string "last_name",  null: false
+    t.string  "first_name", null: false
+    t.string  "last_name",  null: false
+    t.integer "user_id"
   end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
@@ -55,15 +58,13 @@ ActiveRecord::Schema.define(version: 20150610200739) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
-    t.integer  "profile_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["profile_id"], name: "index_users_on_profile_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "comments", "lunches"
   add_foreign_key "comments", "users"
   add_foreign_key "lunches", "users"
-  add_foreign_key "users", "profiles"
+  add_foreign_key "profiles", "users"
 end

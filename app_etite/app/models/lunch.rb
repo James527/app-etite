b0089599d3378673f90799a115bcefff11338371
Walker Flieller <wflieller@gmail.com>
2015-06-10 12:46:27 -0500

    added destroy recursive option for lunches

--- app_etite/app/models/lunch.rb
+++ app_etite/app/models/lunch.rb
@@ -1,4 +1,11 @@
+require_relative 'comment'
 class Lunch < ActiveRecord::Base
+
   has_many :comments
   belongs_to :user
+
+  def destroy_lunch(id)
+  	Comment.destroy_all(:lunch_id => id)
+  	Lunch.destroy_all(:id => id)
+  end
 end

e0b07d7b13d1a19478ec598a7f307a69bd3e8f61
Walker Flieller <wflieller@gmail.com>
2015-06-10 15:24:16 -0500

    fixed errors

--- app_etite/app/controllers/users/registrations_controller.rb
+++ app_etite/app/controllers/users/registrations_controller.rb
@@ -51,8 +51,6 @@ before_filter :configure_account_update_params, only: [:update]
   # The path used after sign up.
   def after_sign_up_path_for(resource)
     super(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || new_profile_path
-  end
   end
 
   # The path used after sign up for inactive accounts.

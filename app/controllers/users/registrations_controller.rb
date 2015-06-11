86edca6f0199f0976b6e367d97982eec19f0175c
Walker Flieller <wflieller@gmail.com>
2015-06-11 11:30:19 -0500

    edited profile routing and views

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 before_filter :configure_sign_up_params, only: [:create]
 before_filter :configure_account_update_params, only: [:update]
-
+def after_sign_up_path_for(resource)
+  redirect_to new_profile_path
+end
 
   # GET /resource/sign_up
   def new
